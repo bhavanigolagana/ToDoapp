cf0ae988da3401debd63a0e580d91d4782bfe462
bhavanigolagana <bhavanigolagana5@gmail.com>
2023-08-22 13:35:15 +0530

    adding ToDo  to online repo

--- todo/admin.py
+++ todo/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from todo.models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display=('task','is_completed','updated_at','created_at')
+    search_fields=('task',)
+admin.site.register(Task,TaskAdmin)
